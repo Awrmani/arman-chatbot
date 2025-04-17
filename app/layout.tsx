1bac3b77f5f383d61d1d80f352727f490978d94e
Awrmani <armanvalaee@gmail.com>
2025-04-16 22:56:28 -0400

    user analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Metadata } from 'next';
 import { Toaster } from 'sonner';
@@ -69,6 +70,7 @@ export default async function RootLayout({
           {children}
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
